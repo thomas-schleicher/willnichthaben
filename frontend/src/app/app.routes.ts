132bab5ed2fc1f21ee9ef2ddd94cee87d0a64e93
Thomas Schleicher <thscleicher@edu.aau.at>
2025-01-15 18:03:19 +0100

    generate sub-page components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { RetailComponent } from './pages/retail/retail.component';
+import { RealestateComponent } from './pages/realestate/realestate.component';
+import { VehicleComponent } from './pages/vehicle/vehicle.component';
 
 export const routes: Routes = [
     { path: "login", component: LoginComponent },
     { path: "signup", component: SignupComponent},
-    // { path: "retail" },
-    // { path: "real-estate" },
-    // { path: "vehicle" },
+    { path: "retail", component: RetailComponent},
+    { path: "real-estate", component: RealestateComponent },
+    { path: "vehicles", component: VehicleComponent },
 ];
---
+++ frontend/src/app/pages/realestate/realestate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realestate',
+  imports: [],
+  templateUrl: './realestate.component.html',
+  styleUrl: './realestate.component.scss'
+})
+export class RealestateComponent {
+
+}
