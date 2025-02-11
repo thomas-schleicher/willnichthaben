0937a3777162e08e178cc373904bb0928f6594e3
Elias Wassertheurer <eliaswa@edu.aau.at>
2025-02-11 15:15:03 +0100

    updated retail SQL script

--- db/scripts/retail_startup.sql
+++ db/scripts/retail_startup.sql
@@ -1,35 +1,37 @@
 -- delete tables if they already exist
-DROP TABLE IF EXISTS marketplace_item_properties CASCADE;
-DROP TABLE IF EXISTS additional_properties CASCADE;
-DROP TABLE IF EXISTS marketplace_items CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS retail_item_properties CASCADE;
+DROP TABLE IF EXISTS retail_categories_properties CASCADE;
+DROP TABLE IF EXISTS retail_items CASCADE;
+DROP TABLE IF EXISTS retail_categories CASCADE;
 
 -- ##################################
 -- #         table creation         #
 -- ##################################
 
 -- main categories
-CREATE TABLE categories
+CREATE TABLE retail_categories
 (
     id   SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
+    name VARCHAR(50) NOT NULL UNIQUE -- retail category label
 );
 
--- additional properties for categories
-CREATE TABLE additional_properties
+-- additional properties for specific categories
+CREATE TABLE retail_categories_properties
 (
     id              SERIAL PRIMARY KEY,
-    name            VARCHAR(50) NOT NULL,
-    category_id     INT REFERENCES categories (id),
-    property_type   VARCHAR(50) DEFAULT 'boolean'
+    name            VARCHAR(50) NOT NULL, -- additional property label for specific category
+    category_id     INT REFERENCES retail_categories (id), -- corresponding retail category by ID
+
+    -- determines the options of which you can choose the property (true/false (2); "56 cm", "M" ... (or more))
+    property_type   VARCHAR(50) DEFAULT 'boolean' 
 );
 
 -- items that are listed
-CREATE TABLE marketplace_items
+CREATE TABLE retail_items
 (
     id                SERIAL PRIMARY KEY,
     name              VARCHAR(100) NOT NULL,
-    category_id       INT          NOT NULL REFERENCES categories (id),
+    category_id       INT          NOT NULL REFERENCES retail_categories (id), -- category by ID
     description       TEXT,
     price             NUMERIC      NOT NULL,
     delivery_options  VARCHAR(100) NOT NULL,
@@ -39,11 +41,11 @@ CREATE TABLE marketplace_items
 );
 
 -- additional info for items
-CREATE TABLE marketplace_item_properties
+CREATE TABLE retail_item_properties
 (
     id                    SERIAL PRIMARY KEY,
-    marketplace_item_id   INT NOT NULL REFERENCES marketplace_items (id) ON DELETE CASCADE,
-    additional_property_id INT NOT NULL REFERENCES additional_properties (id),
+    retail_item_id   INT NOT NULL REFERENCES retail_items (id) ON DELETE CASCADE,
+    additional_property_id INT NOT NULL REFERENCES retail_categories_properties (id),
     value                 TEXT NOT NULL
 );
 
@@ -51,14 +53,14 @@ CREATE TABLE marketplace_item_properties
 -- #        prefill datasets        #
 -- ##################################
 
-INSERT INTO categories (name)
+INSERT INTO retail_categories (name)
 VALUES ('Electronics'),
        ('Furniture'),
        ('Fashion'),
        ('Sports & Outdoors'),
        ('Books & Media');
 
-INSERT INTO additional_properties (name, category_id, property_type)
+INSERT INTO retail_categories_properties (name, category_id, property_type)
 VALUES ('Brand', 1, 'text'),
        ('Model', 1, 'text'),
        ('Color', 2, 'text'),
