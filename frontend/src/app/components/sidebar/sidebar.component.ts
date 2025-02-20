86987c5e356805674f9c1db957609085b2a1b7ff
Raphael <rawalcher@edu.aau.at>
2025-02-20 13:09:52 +0100

    .

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ChatService } from '../../service/chat.service';  // Adjust the import path as needed
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,4 +12,21 @@ export class SidebarComponent {
   @Input() price!: number;
   @Input() seller_name!: string;
   @Input() address!: string;
+  @Input() id!: number;
+
+  constructor(private Router: Router, private chatService: ChatService) {}
+
+  onMessageClick(): void {
+    console.log(this.id);
+
+    this.chatService.sendMessage(this.id, 'I wanted to ask if this is still available?').subscribe({
+      next: (response) => {
+        this.Router.navigateByUrl('/chat');
+        console.log('Message sent successfully', response);
+      },
+      error: (error) => {
+        console.error('Error sending message', error);
+      }
+    });
+  }
 }
